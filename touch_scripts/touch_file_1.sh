30aeb39785b98963b84b642aa28af79008759cbc
napbautista <32447895+napbautista@users.noreply.github.com>
2018-01-11 17:45:09 +0800

    Add files via upload

--- touch_scripts/touch_file_1.sh
+++ touch_scripts/touch_file_1.sh
@@ -4,6 +4,3 @@ echo "Before_Install 123" > /home/ec2-user/Before_Install.txt
 
 . $(dirname $0)/called_script_1.sh
 
-asg=`aws --region us-west-2 autoscaling describe-auto-scaling-instances --instance-ids i-0ec49f13f1a590bb7 --output text --query AutoScalingInstances[0].AutoScalingGroupName`
-
-echo $asg > /home/ec2-user/asg.txt
--- touch_scripts/touch_file_2.sh
+++ touch_scripts/touch_file_2.sh
@@ -4,6 +4,3 @@ echo "After_Install 123" > /home/ec2-user/After_Install.txt
 
 . $(dirname $0)/called_script_2.sh
 
-asg=`aws --region us-west-2 autoscaling describe-auto-scaling-instances --instance-ids i-0ec49f13f1a590bb7 --output text --query AutoScalingInstances[0].AutoScalingGroupName`
-
-echo $asg > /home/ec2-user/asg_2.txt
