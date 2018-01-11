cfc4be8e9b5fc802df9b33ade8432d08a92e69ea
napbautista <32447895+napbautista@users.noreply.github.com>
2018-01-11 16:35:12 +0800

    Add files via upload

--- touch_scripts/touch_file_1.sh
+++ touch_scripts/touch_file_1.sh
@@ -2,4 +2,4 @@
 
 echo "Before_Install 123" > /home/ec2-user/Before_Install.txt
 
-./touch_scripts/called_script_1.sh
+. $(dirname $0)/called_script_1.sh
--- touch_scripts/touch_file_2.sh
+++ touch_scripts/touch_file_2.sh
@@ -2,4 +2,4 @@
 
 echo "After_Install 123" > /home/ec2-user/After_Install.txt
 
-./touch_scripts/called_script_2.sh
+. $(dirname $0)/called_script_2.sh
