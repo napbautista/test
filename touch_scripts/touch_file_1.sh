f575438a7414d70b5aab9bc126c92933ee598f4d
napbautista <32447895+napbautista@users.noreply.github.com>
2018-01-12 10:04:00 +0800

    Add files via upload

--- touch_scripts/touch_file_1.sh
+++ touch_scripts/touch_file_1.sh
@@ -2,6 +2,8 @@
 
 echo "Before_Install 123" > /home/ec2-user/Before_Install.txt
 
+export PATH=~/.local/bin:$PATH
+
 asg=`aws --region us-west-2 autoscaling describe-auto-scaling-instances --instance-ids i-0ec49f13f1a590bb7 --output text --query AutoScalingInstances[0].AutoScalingGroupName`
 
 echo $asg > /home/ec2-user/asg_from_touch_1.txt
